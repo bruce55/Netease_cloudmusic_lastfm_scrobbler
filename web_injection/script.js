d9681970f7bd693a5a0e87c64080586a67825a5f
Bruce Luo <bruce.luo+git@nyu.edu>
2015-06-02 21:23:51 +0800

    First version of the injector

--- web_injection/script.js
+++ web_injection/script.js
@@ -1,1 +1,47 @@
-﻿
+﻿$(document).ready(function () {
+    $("#apiForm").submit(function (e) {
+        e.preventDefault();
+        var form = $(this).serializeArray();
+        var apiInfo = {};
+        form.forEach(function(item) {
+            apiInfo[item.name] = item.value;
+        });
+        console.log(apiInfo);
+        localStorage.apiInfo = JSON.stringify({
+            "token": apiInfo.token,
+            "sk": apiInfo.sk
+        });
+        
+        $.get("js/main.js", function (data) {
+            var script = data;
+            data = data.replace("token: '',", "token: '" + apiInfo.token + "',");
+            data = data.replace("session_key: ''", "session_key: '" + apiInfo.sk + "'");
+            var connection = new WebSocket(apiInfo.address);
+            connection.onopen = function() {
+                connection.send(JSON.stringify({
+                    "id": 0,
+                    "method": "Runtime.evaluate",
+                    "params": {
+                        "expression": data
+                    }
+                }));
+            };
+        },"text");
+
+    });
+    $("#getPortForm").submit(function (e) {
+        e.preventDefault();
+        var port = $("#portInput").val();
+        localStorage.storedPort = port;
+        $("#getAddrIF").attr("src", "http://localhost:" + port + "/json");
+    });
+    if (localStorage.apiInfo) {
+        var stored = JSON.parse(localStorage.apiInfo);
+        $("#tokenInput").val(stored.token);
+        $("#skInput").val(stored.sk);
+    }
+    if (localStorage.storedPort) {
+        $("#portInput").val(localStorage.storedPort);
+        $("#getPortForm").trigger("submit");
+    }
+})
