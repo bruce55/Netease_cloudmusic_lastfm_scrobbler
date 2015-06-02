cbbf15f53cfa89ab24ac40292a2e138427026bc9
Bruce Luo <bruce.luo+git@nyu.edu>
2015-06-02 17:16:42 +0800

    Injector visuals

---
+++ Scripts/_references.js
@@ -0,0 +1,7 @@
+﻿/// <autosync enabled="true" />
+/// <reference path="../js/main.js" />
+/// <reference path="../web_injection/script.js" />
+/// <reference path="jquery-1.9.1.js" />
+/// <reference path="npm.js" />
+/// <reference path="bootstrap.min.js" />
+/// <reference path="bootstrap.js" />
